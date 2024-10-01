b5d018591036d5ae74d22be2dfd2f6b8305f9e49
Roberto <roberto.felipe467@gmail.com>
2024-10-01 20:57:57 -0300

    COmite, Tela de Login

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+
+import Login from './src/Pages/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar backgroundColor="#F28B6C"/>
+      <Login/>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Pages/Login/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { View, StyleSheet, TextInput, Image, TouchableOpacity, Text } from 'react-native';
+
+export default function Login() {
+ return (
+   <View style={styles.container}>
+        <Image style={{marginBottom: "15%"}} source={require("../../Assets/logo.png")} />
+        <TextInput style={styles.input} placeholder='Digite Seu Email:' />
+        <TextInput style={styles.input} placeholder='Digite Sua Senha:' />
+        <TouchableOpacity style={styles.button} >
+            <Text style={{fontSize:24, color: "#FFF", fontWeight:"bold"}} >Entrar</Text>
+        </TouchableOpacity>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor:"#325B70",
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    input:{
+        backgroundColor:"#FFF",
+        width: "90%",
+        height: 60,
+        marginVertical: 8,
+        borderRadius: 12,
+        paddingHorizontal: 12,
+        fontSize: 18,
+        elevation: 10
+    },
+    button:{
+        backgroundColor:"#F28B6C",
+        marginVertical: 8,
+        paddingHorizontal: 24,
+        paddingVertical: 12,
+        borderRadius: 12,
+        elevation: 8
+    }
+
+})
