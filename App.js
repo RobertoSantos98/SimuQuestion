a9f628d2f232e798601873730d4cfd9c7449e6ae
Roberto <roberto.felipe467@gmail.com>
2024-10-09 10:43:45 -0300

    Adicionada a Splash Screen.

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Login from './src/Pages/Login';
 import Routes from './src/Pages/Routes';
+import Splash from './src/Components/Splash';
 
 export default function App() {
 
-  const [ logado, setLogado ] = useState(true)
+  const [ logado, setLogado ] = useState(true);
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 5000); 
+
+    return () => clearTimeout(timer); 
+  }, []);
 
   return (
     <NavigationContainer>
       <StatusBar backgroundColor="#F26A50"/>
-      {logado ? <Routes/> : <Login/>}
+      
+      {showSplash ? (
+        <Splash /> 
+      ) : ( logado ? <Routes /> : <Login /> )}
     </NavigationContainer>
   );
 }
